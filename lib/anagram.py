a0d49d44c891493144f152823b3603230e6443e2
Ngecha <ngechadennis@gmail.com>
2024-09-03 13:01:47 +0300

    assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word 
+
+    def match(self, anagrams):
+        matches = []
+        for name in anagrams:
+            if sorted(name) == sorted(self.word):
+                matches.append(name)
+        return matches
+
